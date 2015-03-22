8e910a5dc6ba84fc53b0c95220ee50bcc03cfaa4
OksanaProtsyk <oksana.protsyk11@yandex.ua>
2015-03-23 00:44:28 +0200

    adding Moddel Order subtypes

---
+++ TPP_MainProject/Models/entities/CustomOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPP_MainProject.Models.entities
+{
+    public class CustomOrder :Order
+    {
+        public String detailDescription { get; set; }
+    }
+}
---
+++ TPP_MainProject/Models/entities/TemplateOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPP_MainProject.Models.entities
+{
+    public class TemplateOrder : Order
+    {
+        public virtual ICollection<ProductItem> orderItems { get; set; }
+    }
+}
