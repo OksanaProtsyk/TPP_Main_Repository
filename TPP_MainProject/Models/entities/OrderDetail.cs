7a06b01d71ffbf31ead56ed91178f93ba4c1539e
OksanaProtsyk <oksana.protsyk11@yandex.ua>
2015-04-06 07:43:35 +0300

    add order details

---
+++ TPP_MainProject/Models/entities/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPP_MainProject.Models.entities
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductItemId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public virtual ProductItem ProductItem { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
