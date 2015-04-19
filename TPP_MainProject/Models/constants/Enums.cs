39e6ee6a76e57bf70f9b24220aae0ccb07b601e1
Yaroslav Propoy <yaroslavpropoy@gmail.com>
2015-04-20 00:32:12 +0300

    update items

--- TPP_MainProject/Models/constants/Enums.cs
+++ TPP_MainProject/Models/constants/Enums.cs
@@ -10,4 +10,5 @@ namespace TPP_MainProject.Models.constants
     public enum TaskStatus { Initial, InProgress, OnHold, Completed};
     public enum ProjectStatus { Initial, InProgress, OnHold, Completed};
     public enum OrderStatus { Initiating, Processiong,Canceled,Rejected, Completed};
+    public enum TemplateSiteTypes { Blog, VisitCard, Oficial, Shop, Amazing };
 }
